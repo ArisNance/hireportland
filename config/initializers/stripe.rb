3d3d6fc1c2713e9795073fabda428bf53e4b4354
Aris Nance <nance.aris@gmail.com>
2017-12-17 04:02:55 +0000

    StripeCredsPush

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,5 +2,6 @@ Rails.configuration.stripe = {
   :publishable_key => ENV['PUBLISHABLE_KEY'],
   :secret_key      => ENV['SECRET_KEY']
 }
-
+PUBLISHABLE_KEY="pk_test_4LDdmwyH6QZh8mndTnPrVxQg"
+SECRET_KEY="sk_test_ckZnOSOpmki1XYMlgWTFQDTW"
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
